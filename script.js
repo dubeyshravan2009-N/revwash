e31ada60ad85a921c1e4874fdcb8c55aec17421e
dubeyshravan2009-N <dubeyshravan2009@gmail.com>
2025-10-11 18:52:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// Simple demo login (no backend yet)
 const loginForm = document.getElementById("loginForm");
 
 loginForm.addEventListener("submit", function(e){
   e.preventDefault();
 
-  // Get user info
   const username = document.getElementById("username").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   if(username && email && password){
     alert(`Welcome, ${username}!`);
-    // After login, you can redirect to main homepage
-    window.location.href = "home.html"; // create this later
+    window.location.href = "home.html";
   } else {
     alert("Please fill all fields");
   }
