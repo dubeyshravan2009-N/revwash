04de3ad43e03ca6f88dfd58f5c3a3354b3213b45
dubeyshravan2009-N <dubeyshravan2009@gmail.com>
2025-10-12 00:47:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
-// Car Models
-const models = {
-    "Toyota":["Corolla","Camry","Fortuner","Innova","Yaris"],
-    "Honda":["City","Civic","Jazz","CR-V","WR-V"],
-    "Hyundai":["i20","i10","Creta","Verna","Venue"],
-    "Mahindra":["XUV700","Thar","Scorpio","Bolero","Marazzo"],
-    "Nissan":["Magnite","Kicks","GT-R","Sunny","X-Trail"],
-    "Tata":["Nexon","Harrier","Tiago","Altroz","Safari"],
-    "Renault":["Kiger","Duster","Triber","Kwid","Captur"],
-    "BMW":["3 Series","5 Series","X1","X5","X6"],
-    "Mercedes":["C-Class","E-Class","GLA","GLE","S-Class"]
+const carModels = {
+  "Toyota": ["Corolla", "Camry", "Fortuner", "Hilux", "Yaris"],
+  "Honda": ["City", "Civic", "Jazz", "WR-V", "CR-V"],
+  "Hyundai": ["i20", "i10", "Creta", "Verna", "Venue"],
+  "Mahindra": ["Thar", "XUV700", "Scorpio", "Bolero", "XUV300"],
+  "Nissan": ["Magnite", "Kicks", "Leaf", "Altima", "GT-R"],
+  "Tata": ["Nexon", "Harrier", "Altroz", "Punch", "Safari"],
+  "Renault": ["Kiger", "Triber", "Kwid", "Duster", "Captur"],
+  "BMW": ["X5", "X3", "3 Series", "5 Series", "7 Series"],
+  "Mercedes": ["A-Class", "C-Class", "E-Class", "GLA", "GLC"]
 };
 
-function selectBrand(brand){
-    localStorage.setItem("selectedBrand",brand);
-    window.location.href="models.html";
+function selectBrand(brand) {
+    localStorage.setItem("selectedBrand", brand);
+    window.location.href = "models.html";
 }
 
-window.onload = function(){
-    const brandTitle=document.getElementById("brandTitle");
-    const modelsContainer=document.getElementById("modelsContainer");
-    const brand=localStorage.getItem("selectedBrand");
-    if(brand && modelsContainer){
-        brandTitle.textContent=brand + " Models";
-        modelsContainer.innerHTML="";
-        models[brand].forEach(m=>{
-            const div=document.createElement("div");
-            div.className="card";
-            div.textContent=m;
-            modelsContainer.appendChild(div);
+window.onload = () => {
+    const brand = localStorage.getItem("selectedBrand");
+    const container = document.getElementById("modelsContainer");
+    if(brand && container) {
+        container.innerHTML = "";
+        carModels[brand].forEach(model => {
+            const div = document.createElement("div");
+            div.classList.add("card");
+            div.innerText = model;
+            container.appendChild(div);
         });
     }
-}
+};
