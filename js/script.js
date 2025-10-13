e6c621371f1059a32ff618e982a15d66a4531acb
dubeyshravan2009-N <dubeyshravan2009@gmail.com>
2025-10-13 13:14:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,49 @@
+// Disable right-click
+document.addEventListener('contextmenu', e => e.preventDefault());
+
+// Car Brands and Models
 const carModels = {
-  "Toyota": ["Corolla", "Camry", "Fortuner", "Hilux", "Yaris"],
-  "Honda": ["City", "Civic", "Jazz", "WR-V", "CR-V"],
-  "Hyundai": ["i20", "i10", "Creta", "Verna", "Venue"],
-  "Mahindra": ["Thar", "XUV700", "Scorpio", "Bolero", "XUV300"],
-  "Nissan": ["Magnite", "Kicks", "Leaf", "Altima", "GT-R"],
-  "Tata": ["Nexon", "Harrier", "Altroz", "Punch", "Safari"],
-  "Renault": ["Kiger", "Triber", "Kwid", "Duster", "Captur"],
-  "BMW": ["X5", "X3", "3 Series", "5 Series", "7 Series"],
-  "Mercedes": ["A-Class", "C-Class", "E-Class", "GLA", "GLC"]
+  "Toyota": ["Corolla","Camry","Fortuner","Hilux","Yaris"],
+  "Honda": ["City","Civic","Jazz","WR-V","CR-V"],
+  "Hyundai": ["i20","i10","Creta","Verna","Venue"],
+  "Mahindra": ["Thar","XUV700","Scorpio","Bolero","XUV300"],
+  "Nissan": ["Magnite","Kicks","Leaf","Altima","GT-R"],
+  "Tata": ["Nexon","Harrier","Altroz","Punch","Safari"],
+  "Renault": ["Kiger","Triber","Kwid","Duster","Captur"],
+  "BMW": ["X5","X3","3 Series","5 Series","7 Series"],
+  "Mercedes": ["A-Class","C-Class","E-Class","GLA","GLC"]
 };
 
-// When a brand is clicked from index.html
-function selectBrand(brand) {
-    localStorage.setItem("selectedBrand", brand);
-    window.location.href = "models.html";
+// Populate brands in index.html
+function populateBrands(){
+  const container = document.getElementById('brandsContainer');
+  container.innerHTML = '';
+  Object.keys(carModels).forEach(brand => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerText = brand;
+    div.addEventListener('click', () => {
+      localStorage.setItem('selectedBrand', brand);
+      window.location.href='models.html';
+    });
+    container.appendChild(div);
+  });
 }
 
-// Populate models dynamically on models.html
-window.onload = () => {
-    const brand = localStorage.getItem("selectedBrand");
-    const container = document.getElementById("modelsContainer");
-    if (brand && container) {
-        container.innerHTML = "";
-        carModels[brand].forEach(model => {
-            const div = document.createElement("div");
-            div.classList.add("card");
-            div.innerText = model;
-            div.addEventListener("click", () => {
-                localStorage.setItem("selectedModel", model);
-                window.location.href = "washType.html";
-            });
-            container.appendChild(div);
-        });
-    }
-};
+// Populate models in models.html
+function populateModels(){
+  const brand = localStorage.getItem('selectedBrand');
+  const container = document.getElementById('modelsContainer');
+  if(!brand || !container) return;
+  container.innerHTML = '';
+  carModels[brand].forEach(model => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerText = model;
+    div.addEventListener('click', () => {
+      localStorage.setItem('selectedModel', model);
+      window.location.href='washType.html';
+    });
+    container.appendChild(div);
+  });
+}
