f16f7dfdbd691e47c2d4c6ba9d6ae465e47e1609
dubeyshravan2009-N <dubeyshravan2009@gmail.com>
2025-10-13 12:59:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,20 +10,26 @@ const carModels = {
   "Mercedes": ["A-Class", "C-Class", "E-Class", "GLA", "GLC"]
 };
 
+// When a brand is clicked from index.html
 function selectBrand(brand) {
     localStorage.setItem("selectedBrand", brand);
     window.location.href = "models.html";
 }
 
+// Populate models dynamically on models.html
 window.onload = () => {
     const brand = localStorage.getItem("selectedBrand");
     const container = document.getElementById("modelsContainer");
-    if(brand && container) {
+    if (brand && container) {
         container.innerHTML = "";
         carModels[brand].forEach(model => {
             const div = document.createElement("div");
             div.classList.add("card");
             div.innerText = model;
+            div.addEventListener("click", () => {
+                localStorage.setItem("selectedModel", model);
+                window.location.href = "washType.html";
+            });
             container.appendChild(div);
         });
     }
